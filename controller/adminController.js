3aa3f6c6c1f456f0cf5aac58f64439c00843f595
krishnadas3 <krishnadasp004@gmail.com>
2024-03-06 20:00:21 +0530

    neww

--- controller/adminController.js
+++ controller/adminController.js
@@ -55,103 +55,103 @@ const loginPostpage = async (req, res) => {
 
 //  categorie management
 
-// let categorieList = async (req, res) => {
-//     try {
-//         let Admin = await admin.find()
-//         if(!Admin){
-//             res.status(400).send('admin not found')
-//         } 
-//         let category = Admin[0].category.map(category => category)
-//         res.render('admin/categorielist',{category})
-//     } catch (error) {
-//         console.error('failed to get categorieist', error)
-//         res.status(500).send('internal server error')
-//     }
-// }
+let categorieList = async (req, res) => {
+    try {
+        let Admin = await admin.find()
+        if(!Admin){
+            res.status(400).send('admin not found')
+        } 
+        let category = Admin[0].category.map(category => category)
+        res.render('admin/categorielist',{category})
+    } catch (error) {
+        console.error('failed to get categorieist', error)
+        res.status(500).send('internal server error')
+    }
+}
 
-//add category post page 
+// add category post page 
 
-// let addCategory = (req,res) => {
-//     res.render('admin/categoriesadd');
-// }
+let addCategory = (req,res) => {
+    res.render('admin/categoriesadd');
+}
 
 
-// let submitAddCategory = async (req,res) => {
-//     let {categoryName} =req.body;
-//     console.log(categoryName);
-//     let Admin = await admin.findOne();
-//     if(!Admin){
-//         res.status(400).send('Admin not found')
-//     }
-//     Admin.category.push({categoryName});
-//    await Admin.save();
-//     res.redirect('/admin/categorielist')
-// }
+let submitAddCategory = async (req,res) => {
+    let {categoryName} =req.body;
+    console.log(categoryName);
+    let Admin = await admin.findOne();
+    if(!Admin){
+        res.status(400).send('Admin not found')
+    }
+    Admin.category.push({categoryName});
+   await Admin.save();
+    res.redirect('/admin/categorielist')
+}
 
-// // edit category here 
+// edit category here 
 
-// let editCategory = async (req,res) => {
-//     let categoryId = req.params.id;
-//     console.log('categoryId : ',categoryId);
-//     try {
-//         let Admin = await admin.findOne();
-//         if(!Admin){
-//             res.status(400).send('Admin Not Found')
-//         }
-//         let category = Admin.category.find(item => item.id == categoryId);
-//         console.log(category);
-//         if(!category){
-//             res.status(400).send('Category Not Found')
-//         }
-//         res.render('admin/categoriesedit', { category });
+let editCategory = async (req,res) => {
+    let categoryId = req.params.id;
+    console.log('categoryId : ',categoryId);
+    try {
+        let Admin = await admin.findOne();
+        if(!Admin){
+            res.status(400).send('Admin Not Found')
+        }
+        let category = Admin.category.find(item => item.id == categoryId);
+        console.log(category);
+        if(!category){
+            res.status(400).send('Category Not Found')
+        }
+        res.render('admin/categoriesedit', { category });
 
-//     } catch (error) {
-//         res.status(500).send('Internal Server Error')
-//     }
-// }
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+}
 
-// let submitEditCategory = async (req,res) => {
-//     let categoryId = req.params.id;
-//     console.log("categoryId  : ",categoryId);
-//     let {categoryName} = req.body;
-//     console.log(categoryName)
-//     try {
-//         let Admin = await admin.findOne();
-//         if(!Admin){
-//             res.status(400).send('Admin Not Found')
-//         }
-//         let categoryInd = await Admin.category.findIndex(item => item.id == categoryId);
-//         console.log("categoryInd : ",categoryInd);
-//         if(categoryInd == -1){
-//             res.status(400).send('Category Not Found')
-//         }else{
-//             Admin.category[categoryInd].categoryName = categoryName ; 
-//             // if i add more things to edit in category i can change it here...
-//             await Admin.save()
-//             res.redirect('/admin/categorielist');
-//         }
-//     } catch (error) {
-//         res.status(500).send('Internal Server Error')
-//     }
-// }
+let submitEditCategory = async (req,res) => {
+    let categoryId = req.params.id;
+    console.log("categoryId  : ",categoryId);
+    let {categoryName} = req.body;
+    console.log(categoryName)
+    try {
+        let Admin = await admin.findOne();
+        if(!Admin){
+            res.status(400).send('Admin Not Found')
+        }
+        let categoryInd = await Admin.category.findIndex(item => item.id == categoryId);
+        console.log("categoryInd : ",categoryInd);
+        if(categoryInd == -1){
+            res.status(400).send('Category Not Found')
+        }else{
+            Admin.category[categoryInd].categoryName = categoryName ; 
+            // if i add more things to edit in category i can change it here...
+            await Admin.save()
+            res.redirect('/admin/categorielist');
+        }
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+}
 
-// // delete category 
+// delete category 
 
-// let deleteCategory = async (req,res) => {
-//     let categoryId = req.params.id;
-//     try {
-//         let Admin = await admin.findOne();
-//         if(!admin){
-//             res.status(400).send('Admin Not Found')
-//         }
-//         Admin.category = Admin.category.filter(item => item.id != categoryId);
-//        await Admin.save();
-//         res.redirect('/admin/categorielist')
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send('Internal server Error')
-//     }
-// }
+let deleteCategory = async (req,res) => {
+    let categoryId = req.params.id;
+    try {
+        let Admin = await admin.findOne();
+        if(!admin){
+            res.status(400).send('Admin Not Found')
+        }
+        Admin.category = Admin.category.filter(item => item.id != categoryId);
+       await Admin.save();
+        res.redirect('/admin/categorielist')
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal server Error')
+    }
+}
 
 
 // subcategory management
@@ -298,12 +298,12 @@ module.exports = {
     loginPostpage,
 
 
-    // categorieList,
-    // addCategory,
-    // submitAddCategory,
-    // editCategory,
-    // submitEditCategory,
-    // deleteCategory,
+    categorieList,
+    addCategory,
+    submitAddCategory,
+    editCategory,
+    submitEditCategory,
+    deleteCategory,
 
     // subCategoryList,
     // deleteSubCategory,
